6611da990185a78aa317e60297bbcbfad12695f4
ilyas <ilyas.ashir96@gmail.com>
2020-09-18 02:27:56 -0400

    completed project

--- client/src/components/Config.js
+++
@@ -1,6 +0,0 @@
-//SERVER ROUTES
-module.exports = {
-  USER_SERVER: "/api/users",
-  sandbox:
-    "AbToAtqykvR0_sRC_azc1F94oI0GvBKt8i5brup_dGZ6KFYvqsWPaxSVQuyMBuuTXsBJDNjsrmu-5xnN",
-};
--- config/key.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://iashir:Malika22!@senecaweb-org4o.mongodb.net/store",
-  accessKeyId: "AKIAVFAC4BW6UCTKPB4K",
-  secretAccessKey: "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",
-  Bucket: "ilyasrecipeappbucket",
-};
